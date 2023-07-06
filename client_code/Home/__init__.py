41b517b929a360f94ddb3e324281f8acee161b5e
brooke@anvil.works <brooke@anvil.works>
2023-07-06 13:11:35 +0000

    Created Home; Edited TestPage; Deleted AnotherPage

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import AnotherPageTemplate
+from ._anvil_designer import HomeTemplate
 from anvil import *
 
-class AnotherPage(AnotherPageTemplate):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
