d98af7c17aff46f2211562f6d2396c673ae2c8bd
char@anvil.works <char@anvil.works>
2023-07-26 15:45:28 +0000

    Edited Components.RadioButton

--- client_code/Components/RadioButton/__init__.py
+++ client_code/Components/RadioButton/__init__.py
@@ -1,32 +1,36 @@
 from ._anvil_designer import RadioButtonTemplate
 from anvil import *
 
-# <div anvil-name="anvil-m3-radio-button-container" class="anvil-m3-radio-button-container">
-#   <div class="anvil-m3-radio-button-hover">
-#     <input class="anvil-m3-radio-button" anvil-name="anvil-m3-radio-button" type="radio">
-#     <span anvil-name="anvil-m3-radio-button-unchecked" class="material-symbols-outlined">radio_button_unchecked</span>
-#     <span anvil-name="anvil-m3-radio-button-checked" class="material-symbols-outlined">radio_button_checked</span>
-#     <label class="anvil-m3-radio-button-label" anvil-name="anvil-m3-radio-button-label"></label>
-#   </div>
-# </div>
-
 class RadioButton(RadioButtonTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-  @property
-  def checked(self):
-    return self._checked
 
-  @checked.setter
-  def checked(self, value):
-    self._checked = value
-    if value:
-      self.dom_nodes['anvil-m3-radio-button-unchecked'].style.display= "none"
-      self.dom_nodes['anvil-m3-radio-button-checked'].style.display= "inline"
-    else:
-      self.dom_nodes['anvil-m3-radio-button-checked'].style.display= "none"
-      self.dom_nodes['anvil-m3-radio-button-unchecked'].style.display= "inline"
-      
+# from ._anvil_designer import CheckboxTemplate
+# from anvil import *
+# from ...Functions import checked_property, innerText_property, disabled_property
+
+# class Checkbox(CheckboxTemplate):
+#   def __init__(self, **properties):
+#     # Set Form properties and Data Bindings.
+#     self.init_components(**properties)
+
+#   disabled = disabled_property('anvil-m3-checkbox')
+
+#   selected = checked_property('anvil-m3-checkbox')
+
+#   label = innerText_property('anvil-m3-checkbox-label')
+
+#   @property
+#   def error(self):
+#     return self._error
+
+#   @error.setter
+#   def error(self, value):
+#     self.dom_nodes['anvil-m3-checkbox-container'].classList.remove('anvil-m3-checkbox-error')
+#     self._error = value
+#     if value:
+#       self.dom_nodes['anvil-m3-checkbox-container'].classList.add('anvil-m3-checkbox-error')
+#       self._error = value
