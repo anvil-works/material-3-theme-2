53c208d819bdd4e555ec5eadc0f59502734f650b
char@anvil.works <char@anvil.works>
2023-08-03 14:36:55 +0000

    Created Components.ButtonMenu.MenuItem; Edited Components.ButtonMenu; Deleted Form1

---
+++ client_code/Components/ButtonMenu/MenuItem/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import MenuItemTemplate
+from anvil import *
+
+class MenuItem(MenuItemTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.dom_nodes['button'].addEventListener("click", self.handle_click)
+
+  def handle_click(self, event):
+    event.preventDefault()
+    self.raise_event("click")
+
+  
