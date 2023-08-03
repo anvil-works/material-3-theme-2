4d2dacd27b99a16c4855e0b9dc5895728630a744
char@anvil.works <char@anvil.works>
2023-08-03 16:03:06 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -3,11 +3,36 @@ from anvil import *
 
 class ButtonMenu(ButtonMenuTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    # TODO: needs an event handler to close when not focused
 
-    # Any code you write here will run before the form opens.
-
+  @property
+  def text(self):
+    return self._text
+  @text.setter
+  def text(self, value):
+    self._text = value
+    self.menu_button.text = value
+  
   def toggle_menu_visibility(self, **event_args):
+    self.set_visibility()
+
+  def set_visibility(self, value = None):
     classes = self.dom_nodes['anvil-m3-buttonMenu-items-container'].classList
-    classes.toggle('anvil-m3-buttonMenu-items-hidden')
+    if value:
+      classes.toggle('anvil-m3-buttonMenu-items-hidden', value)
+    else:
+      classes.toggle('anvil-m3-buttonMenu-items-hidden')
+
+  def _anvil_get_design_info_(self, as_layout=False):
+    di = super()._anvil_get_design_info_(as_layout)
+    di['interactions'] = [{
+      "type": "whole_component",
+      "title": "Edit text",
+      "icon": "edit",
+      "default": True,
+      "callbacks": {
+        "execute": lambda: anvil.designer.start_inline_editing(self, "text", self.dom_nodes['anvil-m3-radiobutton-label'])
+      }
+    }]
+    return di 
