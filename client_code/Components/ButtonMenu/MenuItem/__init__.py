ba5034305dda4b7a9dae057293741c1731dc34ba
char@anvil.works <char@anvil.works>
2023-08-03 16:27:36 +0000

    Edited anvil-m3/menu.css, Components.ButtonMenu and Components.ButtonMenu.MenuItem

--- client_code/Components/ButtonMenu/MenuItem/__init__.py
+++ client_code/Components/ButtonMenu/MenuItem/__init__.py
@@ -5,7 +5,7 @@ class MenuItem(MenuItemTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.dom_nodes['button'].addEventListener("click", self.handle_click)
+    # self.dom_nodes['button'].addEventListener("click", self.handle_click)
 
   def handle_click(self, event):
     event.preventDefault()
