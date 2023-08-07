33cc4a6f8e10f1e61ef00387a48cc3e9fdad7b17
char@anvil.works <char@anvil.works>
2023-08-07 13:38:56 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -77,16 +77,6 @@ class ButtonMenu(ButtonMenuTemplate):
           "onSelectOther": self._on_select_other
         }
       },
-      # { # TODO: get this to work so you can edit the text in the button
-      # "type": "whole_component",
-      # "title": "Edit button text",
-      # "icon": "edit",
-      # "callbacks": 
-      #   {
-      #     "execute": lambda: anvil.designer.start_inline_editing(self.menu_button, "text", self.menu_button.dom_nodes['anvil-m3-button-text'])
-      #   }
-      # }
-    ]
     return design_info
 
   def _on_select_descendant(self):
