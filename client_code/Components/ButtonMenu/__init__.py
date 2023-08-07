352e6b0a1f8415180f34919feef0c842804f6c7e
char@anvil.works <char@anvil.works>
2023-08-07 16:29:26 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -39,7 +39,7 @@ class ButtonMenu(ButtonMenuTemplate):
   def menuOpen(self, value):
     self._menuOpen = value or False
     if value:
-      self.dom_nodes['anvil-m3-buttonMenu-container'].addEventListener('click')
+      self.dom_nodes['anvil-m3-buttonMenu-container'].addEventListener('focus')
 
   @property
   def enabled(self):
