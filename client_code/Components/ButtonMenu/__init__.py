cc4f83edc7e271e9f9687fdcfbf3308dcb8cbba0
char@anvil.works <char@anvil.works>
2023-08-07 14:02:38 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -73,7 +73,7 @@ class ButtonMenu(ButtonMenuTemplate):
       {
         "type": "on_selection",
         "callbacks": {
-          "onSelect": self._on_select,
+          # "onSelect": self._on_select,
           "onSelectDescendent": self._on_select_descendant, 
           "onSelectOther": self._on_select_other
         }
