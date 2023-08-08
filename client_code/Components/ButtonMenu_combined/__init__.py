23ded4dc2cea00b1c409e2ade37e43781c301b7d
char@anvil.works <char@anvil.works>
2023-08-08 15:28:05 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -56,6 +56,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
   def position(self, value = {"top": 0, "left": 0, "bottom": 0, "right": 0}):
     self._position = value
     self.windowSize = {"width": window.innerWidth, "height": window.innerHeight}
+    # 
 
   @property
   def windowSize(self):
