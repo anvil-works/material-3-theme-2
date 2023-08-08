c2d4bbae608a5baa825bbd7e246a6e907ca2d186
char@anvil.works <char@anvil.works>
2023-08-08 09:48:53 +0000

    Edited Components.Menu.MenuItem

--- client_code/Components/Menu/MenuItem/__init__.py
+++ client_code/Components/Menu/MenuItem/__init__.py
@@ -7,7 +7,7 @@ class MenuItem(MenuItemTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # self.dom_nodes['button'].addEventListener("click", self.handle_click)
+    self.dom_nodes['button'].addEventListener("click", self.handle_click)
 
   @property
   def leading_icon(self):
