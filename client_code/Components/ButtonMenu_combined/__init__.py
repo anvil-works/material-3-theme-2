af6b92a998447a641640866c7e25c6779264ee39
char@anvil.works <char@anvil.works>
2023-08-08 14:02:52 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -94,6 +94,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
     self.set_visibility(False)
 
   def get_button_position(self):
+    self.menu_button
     """This method is called when the form is shown on the page"""
     pass
 
