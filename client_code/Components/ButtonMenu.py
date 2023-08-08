696efc2adbad654a18c2408f315e90b0bea0b0c3
char@anvil.works <char@anvil.works>
2023-08-08 12:25:13 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu.py
+++ client_code/Components/ButtonMenu.py
@@ -3,6 +3,6 @@ from anvil.js import document
 
 class ButtonMenu(Container):
   def __init__(self, **properties):
-    self.dom_element = document.createElement
+    self.dom_element = document.createElement(div)
 
   
