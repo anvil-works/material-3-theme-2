5a0fe02af57bb775be1d3d1d1eda3519ed0e0879
char@anvil.works <char@anvil.works>
2023-08-08 15:34:28 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -74,10 +74,11 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
     self._menuSize = value
 
   def toggle_menu_visibility(self, **event_args):
-    self.set_visibility()
     
     self.get_button_position() #TODO: Will really only need to get this if visible is true
     self.printPosition()
+    
+    self.set_visibility()
     
     self.menuOpen = not self.dom_nodes['anvil-m3-buttonMenu-items-container'].classList.contains('anvil-m3-buttonMenu-items-hidden')
 
