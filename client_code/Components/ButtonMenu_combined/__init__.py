e2248ac53282308f077ccac0b5034ac64e056a3a
char@anvil.works <char@anvil.works>
2023-08-08 14:21:20 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -102,7 +102,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
       "left": rect.left
     }
     print(positionp["top"])
-    position
+    self.position = 
     """This method is called when the form is shown on the page"""
     pass
 
