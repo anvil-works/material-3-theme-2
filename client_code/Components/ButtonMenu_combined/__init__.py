e6081f995e5d3b6934b8bd787283cd35786001eb
char@anvil.works <char@anvil.works>
2023-08-08 14:37:34 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -59,7 +59,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
     self.set_visibility()
     self.menuOpen = not self.dom_nodes['anvil-m3-buttonMenu-items-container'].classList.contains('anvil-m3-buttonMenu-items-hidden')
     self.get_button_position()
-    self.printPosition()
+    # self.printPosition()
 
   def set_visibility(self, value = None):
     classes = self.dom_nodes['anvil-m3-buttonMenu-items-container'].classList
