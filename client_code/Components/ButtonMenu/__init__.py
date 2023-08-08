7e3f24b0e514bb63a5636192f9a4ff00774c1e35
char@anvil.works <char@anvil.works>
2023-08-08 11:02:07 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -3,7 +3,7 @@ from ._anvil_designer import ButtonMenuTemplate
 from anvil import *
 from anvil.js.window import document
 import random, string
-from Me
+from Menu import Menu
 
 class ButtonMenu(ButtonMenuTemplate):
   def __init__(self, **properties):
