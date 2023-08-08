7b04e6ad8bd9f661041fa32ca4b00f7dc633c921
char@anvil.works <char@anvil.works>
2023-08-08 14:41:36 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -57,7 +57,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
     self._position = value
 
   @property
-  def window
+  def window(self):
 
   def toggle_menu_visibility(self, **event_args):
     self.set_visibility()
