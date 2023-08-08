e323d9686f8248c57f46a727753f575f4de9ed72
char@anvil.works <char@anvil.works>
2023-08-08 14:16:43 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -94,7 +94,8 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
     self.set_visibility(False)
 
   def get_button_position(self):
-    self.menu_button.dom_nodes['anvil-m3-button'].getBoundingClientRect()
+    rect = self.menu_button.dom_nodes['anvil-m3-button'].getBoundingClientRect()
+    
     """This method is called when the form is shown on the page"""
     pass
 
