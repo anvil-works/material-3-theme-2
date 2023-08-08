3b481226382484fc4ee8fcf52a7e5f996afb03db
char@anvil.works <char@anvil.works>
2023-08-08 11:20:11 +0000

    Edited Components.ButtonMenu

--- client_code/Components/ButtonMenu/__init__.py
+++ client_code/Components/ButtonMenu/__init__.py
@@ -49,7 +49,7 @@ class ButtonMenu(ButtonMenuTemplate):
   def set_visibility(self, value = None):
     # classes = self.menu_button.dom_nodes['anvil-m3-buttonMenu-items-container'].classList
     if value is not None:
-      self.menu_button.visible = 
+      self.menu_button.visible = value
       # classes.toggle('anvil-m3-buttonMenu-items-hidden', not value)
     else:
       pass
