b2cd5e1c974edbfcdb0e4fb0639f8a0ae4e65be1
char@anvil.works <char@anvil.works>
2023-08-08 11:22:52 +0000

    Edited Components.Menu

--- client_code/Components/Menu/__init__.py
+++ client_code/Components/Menu/__init__.py
@@ -23,7 +23,7 @@ class Menu(MenuTemplate):
 
   def set_or_toggle_visibility(self, value = None):
     # classes = self.dom_nodes['anvil-m3-menu-items-container'].classList
-    if value not None:
+    if value is not None:
       self.visible = value
       # classes.toggle('anvil-m3-menu-hidden', not value)
     else:
