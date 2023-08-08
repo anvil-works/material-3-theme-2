786399f12f63d1052c6dc48d01be121e63fb5c16
char@anvil.works <char@anvil.works>
2023-08-08 09:02:42 +0000

    Created menu_test_page; Deleted Form1

--- client_code/menu_test_page/__init__.py
+++ client_code/menu_test_page/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import menu_test_pageTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class menu_test_page(menu_test_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
