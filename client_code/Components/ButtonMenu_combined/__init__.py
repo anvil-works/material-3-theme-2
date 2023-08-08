1ba692bea37cfc1b2523adb15ebffe2f9ddc5fb8
char@anvil.works <char@anvil.works>
2023-08-08 14:19:37 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -99,6 +99,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
       "top": rect.top,
       "right": rect.right,
       "bottom": rect.bottom,
+      ""
     }
     """This method is called when the form is shown on the page"""
     pass
