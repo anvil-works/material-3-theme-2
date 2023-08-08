41d917aa879ecbf64897aab844008959a5dad61b
char@anvil.works <char@anvil.works>
2023-08-08 09:11:56 +0000

    Edited settings; Deleted menu_test_page

--- client_code/menu_test_page/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import menu_test_pageTemplate
-from anvil import *
-
-class menu_test_page(menu_test_pageTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
