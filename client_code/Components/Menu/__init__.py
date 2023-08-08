2bf899dce57d8aa1f07d53388d24dd9c5ee588f7
char@anvil.works <char@anvil.works>
2023-08-08 11:23:08 +0000

    Edited Components.Menu

--- client_code/Components/Menu/__init__.py
+++ client_code/Components/Menu/__init__.py
@@ -27,5 +27,5 @@ class Menu(MenuTemplate):
       self.visible = value
       # classes.toggle('anvil-m3-menu-hidden', not value)
     else:
-      pass
+      currVal = 
       # classes.toggle('anvil-m3-menu-hidden')
