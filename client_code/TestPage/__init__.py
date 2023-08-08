14bc753c3b020548a507e531ed208dc340613306
char@anvil.works <char@anvil.works>
2023-08-08 12:57:13 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -6,3 +6,7 @@ class TestPage(TestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+  def menu_item_1_click(self, **event_args):
+    pass
+
