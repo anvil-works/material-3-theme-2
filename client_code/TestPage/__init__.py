8a31dab30a69fb8f5997b676ebf8a2ec46735601
char@anvil.works <char@anvil.works>
2023-08-08 10:51:28 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -6,17 +6,3 @@ class TestPage(TestPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-  def button_1_click(self, **event_args):
-    """This method is called when the component is clicked"""
-    self.checkbox_4.focus()
-
-
-
-  
-
-  
-
-
-
-
