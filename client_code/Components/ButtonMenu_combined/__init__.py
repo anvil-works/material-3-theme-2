7968a2d572196e4355483ce6034f8debfb157860
char@anvil.works <char@anvil.works>
2023-08-08 14:43:45 +0000

    Edited Components.ButtonMenu_combined

--- client_code/Components/ButtonMenu_combined/__init__.py
+++ client_code/Components/ButtonMenu_combined/__init__.py
@@ -60,7 +60,7 @@ class ButtonMenu_combined(ButtonMenu_combinedTemplate):
   def windowSize(self):
     return self._windowSize
   @windowSize.setter
-  def windowSize(self, value);
+  def windowSize(self, value):
     self._windowSize = value
 
   def toggle_menu_visibility(self, **event_args):
