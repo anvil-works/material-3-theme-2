5e1ecb73f05a2a1eb552795cf1293114a4d2d7bf
char@anvil.works <char@anvil.works>
2023-08-08 11:39:06 +0000

    Edited Components.Menu.MenuItem

--- client_code/Components/Menu/MenuItem/__init__.py
+++ client_code/Components/Menu/MenuItem/__init__.py
@@ -9,6 +9,7 @@ class MenuItem(MenuItemTemplate):
     self.init_components(**properties)
     self.dom_nodes['anvil-m3-menuItem-container'].addEventListener("click", self.handle_click)
 
+  """ Properties """
   @property
   def leading_icon(self):
     return self._leading_icon
