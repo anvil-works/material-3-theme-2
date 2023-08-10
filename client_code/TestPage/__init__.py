6fc4e1617adb2b4a4200133fd4c799aeb6511e6e
char@anvil.works <char@anvil.works>
2023-08-10 13:31:46 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -7,8 +7,32 @@ class TestPage(TestPageTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def menu_item_1_click(self, **event_args):
-    alert("YOU CLICKED THE ITEM! IT'S A PERSON!")
+  def select_font(self, value):
+    print(f"font face {value} selected")
 
   def menu_item_2_click(self, **event_args):
-    alert("YOU CLICKED THE ITEM! IT'S PEOPLE!")
+    self.select_font(self.menu_item_2.text)
+
+  def menu_item_3_click(self, **event_args):
+    self.select_font(self.menu_item_3.text)
+
+  def menu_item_10_click(self, **event_args):
+    self.select_font(self.menu_item_10.text)
+
+  def menu_item_12_click(self, **event_args):
+    self.select_font(self.menu_item_12.text)
+
+  def menu_item_8_click(self, **event_args):
+    self.select_font(self.menu_item_8.text)
+
+  def menu_item_11_click(self, **event_args):
+    self.select_font(self.menu_item_11.text)
+
+  def menu_item_9_click(self, **event_args):
+    self.select_font(self.menu_item_9.text)
+
+
+
+
+
+
