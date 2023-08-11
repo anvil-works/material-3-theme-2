6d2834fa3fe9ae10c4a946fa6a63b8533671a6bf
char@anvil.works <char@anvil.works>
2023-08-11 10:34:41 +0000

    Edited TestPage

--- client_code/TestPage/__init__.py
+++ client_code/TestPage/__init__.py
@@ -8,7 +8,7 @@ class TestPage(TestPageTemplate):
     self.init_components(**properties)
 
   def radio_button_1_click(self, **event_args):
-    
+    g
     
   def radio_button_2_click(self, **event_args):
     pass
