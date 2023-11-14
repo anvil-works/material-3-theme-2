f9c254cb2b4baede89a88b02a74d87a475561e59
char@anvil.works <char@anvil.works>
2023-11-14 19:56:49 +0000

    Edited Storybooks.Card, Components.Card and anvil-m3/card.css

--- client_code/Components/Card/__init__.py
+++ client_code/Components/Card/__init__.py
@@ -15,10 +15,34 @@ class Card(CardTemplate):
     # Any code you write here will run before the form opens.
   align = style_property('anvil-m3-card-component', 'justifyContent', 'align')
   visible = HtmlTemplate.visible
+
+  def set_image_position(self, value):
+    self.dom_nodes['anvil-m3-card-image'].style = ""
+    self.dom_nodes['anvil-m3-card-content'].style = ""
+    self.dom_nodes['anvil-m3-card-shadow'].style = ""
+    if value is "None":
+      self.dom_nodes['anvil-m3-card-image'].style = "display: none;"
+    elif value is "Top":
+      self.dom_nodes['anvil-m3-card-shadow'].style = "flex-direction: column;"
+      self.dom_nodes['anvil-m3-card-image'].style = "width: 100%;"
+    elif value is "Bottom":
+      self.dom_nodes['anvil-m3-card-shadow'].style = "flex-direction: column-reverse;"
+      self.dom_nodes['anvil-m3-card-image'].style = "width: 100%;"
+    elif value is "Left":
+      self.dom_nodes['anvil-m3-card-shadow'].style = "flex-direction: row;"
+      self.dom_nodes['anvil-m3-card-image'].style = "height: 100%;"
+    elif value is "Right":
+      self.dom_nodes['anvil-m3-card-shadow'].style = "flex-direction: row-reverse;"
+      self.dom_nodes['anvil-m3-card-image'].style = "height: 100%;"
+    elif value is "Full":
+      self.dom_nodes['anvil-m3-card-content'].style = "display: none;"
+      # self.dom_nodes['anvil-m3-card-image'].style = "width: 100%; height: 100%;"
+  image_position = property_with_callback("image_position", set_image_position)
   
   def set_appearance(self, value):
-    self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-outlined', False)
-    self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-filled', False)
-    self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-elevated', False)
-    self.dom_nodes['anvil-m3-card'].classList.toggle(f'anvil-m3-{value}', True)
+    pass
+    # self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-outlined', False)
+    # self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-filled', False)
+    # self.dom_nodes['anvil-m3-card'].classList.toggle('anvil-m3-elevated', False)
+    # self.dom_nodes['anvil-m3-card'].classList.toggle(f'anvil-m3-{value}', True)
   appearance = property_with_callback("appearance", set_appearance)
