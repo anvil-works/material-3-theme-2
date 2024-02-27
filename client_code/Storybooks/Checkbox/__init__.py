1953e2b96cef1bf757a1dcb88d0cbd4dd0ddfae3
patricia@anvil.works <patricia@anvil.works>
2024-02-27 11:34:59 +0000

    Edited settings and Storybooks.Checkbox

--- client_code/Storybooks/Checkbox/__init__.py
+++ client_code/Storybooks/Checkbox/__init__.py
@@ -9,8 +9,5 @@ class Checkbox(CheckboxTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.checkbox_3.checked = None
-    self.checkbox_5.checked = None
-    self.checkbox_9.checked = None
 
     # Any code you write here will run before the form opens.
