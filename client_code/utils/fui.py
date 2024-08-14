099119fd3266fbad81ddc811a7c747a2c07fe79c
char@anvil.works <char@anvil.works>
2024-08-14 10:00:21 +0000

    Edited utils.fui, Storybooks.Dropdowns_copy and anvil-m3/menu.css

--- client_code/utils/fui.py
+++ client_code/utils/fui.py
@@ -47,15 +47,19 @@ def auto_update(
     el_height = floating_el.offsetHeight
     print(rv.placement)
     if 'bottom' in rv.placement:
-      print("HELLO")
+      print("BOOTTTTTOM")
       available_space = window.innerHeight - reference_el.getBoundingClientRect().bottom
       if available_space < el_height:
+        print("need to resize")
         floating_el.style.bottom = f"{0}px"
+        floating_el.style.height = f"{300}px"
       else:
+        print("this is fine!")
         floating_el.style.top = f"{rv.y}px"
     else:
+      print("T-O-P TOP!")
       available_space = reference_el.getBoundingClientRect().top
-      print(available_space)
+      # print(available_space)
       # if available_space < el_height:
         # floating_el.style.bottom = f"{10}px"
         
