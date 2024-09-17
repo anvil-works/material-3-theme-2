e9d6d691667d369c4e464016e72d33cb90baeedb
char@anvil.works <char@anvil.works>
2024-09-17 10:50:11 +0000

    Edited _Components.RadioGroup

--- client_code/_Components/RadioGroup/__init__.py
+++ client_code/_Components/RadioGroup/__init__.py
@@ -1,14 +1,24 @@
 from ._anvil_designer import RadioGroupTemplate
 from anvil import *
-import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-
+from ...Functions import property_with_callback, property_without_callback, margin_property, color_property
+from anvil.js import window, get_dom_node
+from anvil.js.window import document
+import random, string, math
+from ...utils import gen_id
+import anvil.designer
 
 class RadioGroup(RadioGroupTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  def _set_group_name(self, value):
+    pass
+  group_name = property_with_callback("text", _set_text)
+
+
+
+# <div anvil-name="anvil-m3-radiogroup-component" style="display:flex">
+#   <div anvil-name="anvil-m3-radiogroup-container" class="anvil-m3-radiogroup-container">
+#   </div>
+# </div>
