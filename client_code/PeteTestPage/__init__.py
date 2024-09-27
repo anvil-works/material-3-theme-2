fe5037ee198bdde4c36ad913c87f994b9daa955c
Pete Burgers <pete@anvil.works>
2024-09-27 10:12:37 +0100

    test: Add "Select Bruce" button

--- client_code/PeteTestPage/__init__.py
+++ client_code/PeteTestPage/__init__.py
@@ -15,5 +15,12 @@ class PeteTestPage(PeteTestPageTemplate):
 
   def radio_group_panel_1_change(self, **event_args):
     """This method is called when the RadioButton selection changes"""
-    print(self.radio_group_panel_1.selected_button.text)
+    selected = self.radio_group_panel_1.selected_button
+    print(f"Change: {selected.text} ({selected.value})")
+
+  def checkbox_1_change(self, **event_args):
+    print(self.checkbox_1.checked)
+
+  def button_1_click(self, **event_args):
+    self.radio_group_panel_2.select(self.radio_button_4)
     
