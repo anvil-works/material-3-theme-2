cf3f7c50f9f9daa30329ee9c4eee07d33d929fe0
brooke@anvil.works <brooke@anvil.works>
2024-10-23 12:50:33 +0000

    Review comments

--- client_code/SidesheetContent/__init__.py
+++ client_code/SidesheetContent/__init__.py
@@ -5,6 +5,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+#TODO: add to _Components
+#TODO: in toolbox item yaml, add heading and icon button to appropriate slots as a composite component
+
 class SidesheetContent(SidesheetContentTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
--- client_code/_Components/__init__.py
+++ client_code/_Components/__init__.py
@@ -1,4 +1,2 @@
-import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+#TODO: replace all @property usages and everything that uses property_with_callback and property_without_callback with @anvil_prop
+#TODO: move event listeners to _init_ (unless they are special)
