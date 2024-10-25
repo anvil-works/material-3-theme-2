6188c54f8623660605e4d4b031b6bfc0b5d4e592
patricia@anvil.works <patricia@anvil.works>
2024-10-25 13:59:30 +0000

    Edited _Components.ButtonMenu and _Components.Button

--- client_code/_Components/Button/__init__.py
+++ client_code/_Components/Button/__init__.py
@@ -6,11 +6,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.js
 from ...Functions import color_property, role_property, tooltip_property, custom_bold_property, font_family_property, font_size_property, style_property, property_with_callback, underline_property, bold_property, italic_property, spacing_property 
-from ...utils.properties import get_unset_value, get_unset_spacing
+from ...utils.properties import get_unset_value, get_unset_spacing, ComponentTag, anvil_prop
 from anvil import HtmlTemplate
 import anvil.designer
-from ...utils.properties import ComponentTag
-from ...utils.properties import anvil_prop
 
 class Button(ButtonTemplate):
   def __init__(self, **properties):
