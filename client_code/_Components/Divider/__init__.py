6e0be4c7ef1024b41d4b3cc9d021b78ff126e8d4
patricia@anvil.works <patricia@anvil.works>
2024-10-25 15:30:33 +0000

    Edited _Components.Divider, _Components.DropdownMenu, Form1 and _Components.CircularProgressIndicator

--- client_code/_Components/Divider/__init__.py
+++ client_code/_Components/Divider/__init__.py
@@ -5,7 +5,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from ...Functions import color_property, role_property, margin_property
-from ...utils.properties import get_unset_margin
+from ...utils.properties import get_unset_margin, anvil_prop
 
 class Divider(DividerTemplate):
   def __init__(self, **properties):
@@ -32,15 +32,10 @@ class Divider(DividerTemplate):
   color = color_property('anvil-m3-divider', 'border-color', 'color')
   role = role_property('anvil-m3-divider')
   margin = margin_property('anvil-m3-divider')
-  
-  @property
-  def type(self):
-    return self._props.get('type')
 
-  @type.setter
+  @anvil_prop
   def type(self, value):
     divider = self.dom_nodes['anvil-m3-divider']
-    self._props['type'] = value
     value = value.lower().replace(' ', '-')
     divider.className = "anvil-m3-divider"
     self.dom_nodes['anvil-m3-divider'].classList.add(value)
